b6261946d996ab135993937ff92583e698b30be8
vaishnavi <vkawale2004@gmail.com>
2022-12-04 08:45:19 +0530

    update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
 
 const availableNotes = [2000, 500, 700, 20, 10, 5, 1];
 
@@ -12,9 +13,7 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
             const amountToBeReturned = cashGiven.value - billAmount.value;
             calculateChange(amountToBeReturned);
         } else {
-        showMessage(
-            "do you wanna wash plates?"
-            );
+        showMessage("do you wanna wash plates?");
         }
     } else {
         showMessage("Invalid Bill Amount");
@@ -22,7 +21,14 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
 });
 
 function calculateChange(amountToBeReturned) {
-    const 
+
+    for (let i = 0; i < availableNotes.length; i+=1) {
+        const numberOfNotes = Math.trunc(
+            amountToBeReturned / availableNotes[i]
+        );
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
+        noOfNotes[i].innerText = noOfNotes;
+    }
 }
 
 function hideMessage() {
@@ -33,11 +39,3 @@ function showMessage(msg) {
     message.style.display = "block";  
     message.innerText = msg;
 }
-
-
-
-
-
-
-
-
